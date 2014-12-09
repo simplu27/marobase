3eda07b0981e7cc7cc7daf3cf5e8df52a5c902c9
simplu27 <simplu27@yahoo.com>
2014-12-09 16:07:25 +0200

    Create footer.inc.php

---
+++ inc/footer.inc.php
@@ -0,0 +1,6 @@
+<hr>
+<p id="copyright">Copyright &copy; <?php echo date('Y'); ?> | <a href="<?php echo $root;?>"><?php echo $siteName;?>.</a> All Rights Reserved</p>
+<!-- JS -->
+<script src="<?php echo $root_js;?>jquery.js"></script>
+</body>
+</html>
