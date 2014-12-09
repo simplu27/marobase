44bbf6d87345ffaa340982cf7061cfa5c9b9b3b4
simplu27 <simplu27@yahoo.com>
2014-12-09 16:05:57 +0200

    Create index.php

---
+++ contact/index.php
@@ -0,0 +1,12 @@
+<?php 
+include_once ('../inc/config.php');
+$pageId = 'contacttPage';
+$pageTitle = "Contact page";
+$pageDesc = "Page Description";
+
+include_once (INC.'header.inc.php');
+?>
+
+<p>Content for <?php echo $pageTitle;?></p>
+
+<?php include_once (INC.'footer.inc.php');?>
