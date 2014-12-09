a61ee45c8935b90bf555dc0e43e033c2b385ba07
simplu27 <simplu27@yahoo.com>
2014-12-09 16:08:32 +0200

    Create menu.inc.php

---
+++ inc/menu.inc.php
@@ -0,0 +1,8 @@
+<div id="nav">
+	<ul>
+		<li><a <?php echo ($pageId == 'homePage'? 'class="active" ':''); ?>href="<?php echo $root;?>">Home</a></li>
+		<li><a <?php echo ($pageId == 'aboutPage'? 'class="active" ':''); ?>href="<?php echo $root;?>about/">About</a></li>
+		<li><a <?php echo ($pageId == 'portfolioPage'? 'class="active" ':''); ?>href="<?php echo $root;?>portfolio/">Portfolio</a></li>
+		<li><a <?php echo ($pageId == 'contactPage'? 'class="active" ':''); ?>href="<?php echo $root;?>contact/">Contact</a></li>
+	</ul>
+</div><!-- nav -->
