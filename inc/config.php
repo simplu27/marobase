25bbc79f4142d10adf4f86b3a09e5b77a5ccf075
simplu27 <simplu27@yahoo.com>
2014-12-09 16:06:48 +0200

    Create config.php

---
+++ inc/config.php
@@ -0,0 +1,17 @@
+<?php
+// global site name and tagline
+$siteName = 'aNew2';
+$siteTagline = 'Base Structure Template PHP';
+
+// root folder
+$root = '/anew/';
+
+// root for css and js
+$root_css = $root."asset/css/";
+$root_js = $root."asset/js/";
+
+// root for images
+$images = $root."images/";
+
+// include folder
+define ("INC", $_SERVER['DOCUMENT_ROOT']."anew/inc/");
